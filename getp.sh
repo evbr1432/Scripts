c5eb20390b6740a079790240588aa7ebe2ad1260
evbr1432 <evbr1432@colorado.edu>
2016-09-01 09:03:02 -0400

    Added variable to disk percetage

--- getp.sh
+++ getp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 getp (){
 df|awk '{print $5}'|awk 'NR==2{print $1}'|grep -Eo '[0-9]{1,3}';}
-if [[ getp -gt 10 ]]; then
-	echo "Your space is over 80%. Please delete some items" | mail evbr1432@colorado.edu
+getp
+DiskPerct=80
+if [[ getp -gt $DiskPerct ]]; then
+	echo "Your space is over $DiskPerct. Please delete some items" | mail evbr1432@colorado.edu
 fi
 
 
