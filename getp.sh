2244696a25aceab6cc91afda35ad408246da4c99
evbr1432 <evbr1432@colorado.edu>
2016-09-01 13:17:52 -0400

    Added some comments

--- getp.sh
+++ getp.sh
@@ -6,6 +6,7 @@ df|awk '{print $5}'|awk 'NR==2{print $1}'|grep -Eo '[0-9]{1,3}';
 }
 
 DiskPerct=80
+#I would run this as a cron job every hour so the script doesn't have to always be running in the background. If needed, you could change this to a while loop bt put sleeps inside the loop to make the system wait to send the next emaill.
 if [[ getp -gt $DiskPerct ]]; then
 	echo "Your space is over $DiskPerct. Please delete some items" | mail example@yahoo.com
 fi
